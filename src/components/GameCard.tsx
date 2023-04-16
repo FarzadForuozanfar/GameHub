1878a6cfbb2f5b635b14a93ea59d8c69f0c404aa
farzad foruozanfar <foruozanfar2000f@gmail.com>
2023-04-16 22:48:54 +0330

    get optimize poster card img

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Game } from "../hooks/useGames"
 import { array } from "zod"
 import PlatformIconList from './PlatformIconList';
 import CriticScore from "./CriticScore";
+import getCropedImageUrl from "../services/image-url";
 
 interface Props {
     game: Game
@@ -11,7 +12,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
     return (
         <Card borderRadius={10} overflow={'hidden'}>
-            <Image src={game.background_image} />
+            <Image src={getCropedImageUrl(game.background_image)} />
             <CardBody>
                 <Heading fontSize={'2xl'}>{game.name}</Heading>
                 <HStack justifyContent={'space-between'}>
