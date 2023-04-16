71dec49ef0c401b058241471960d77f3089eddec
farzad foruozanfar <foruozanfar2000f@gmail.com>
2023-04-16 15:41:45 +0330

    build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 
 import { useState } from 'react'
 import './App.css'
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react'
 function App() {
 
   return (
-    <div>
-      
-    </div>
+    <Grid templateAreas={{
+      base : `"nav" "main"`,
+      lg: `"nav nav" "aside main"`,
+    }}>
+      <GridItem area='nav' bg='gold'>nav</GridItem>
+      <Show above='lg'>
+        <GridItem area='aside' bg='coral'>aside</GridItem>
+      </Show>
+      <GridItem area='main' bg='twitter.300'>main5</GridItem>
+    </Grid>
   )
 }
 
